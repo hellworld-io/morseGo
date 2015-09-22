bd9fb1bf1fefc8f755e2223730b45325bcfabf59
YoonJungBoo <coma333ryu@gmail.com>
2015-09-21 21:37:40 -0400

    no message

--- morse.go
+++ morse.go
@@ -5,79 +5,34 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"log"
-	"os"
+	"flag"
 )
 
-
-type morseCode struct {
-	code string
-	text string
-}
-
-type YourJson struct {
-	YourSample []struct {
-		data map[string]string
-	}
-}
-
-func convertToMorseByString(strInput string) string{
-
-	// Alphabet to Morse Code
-	if strInput != "" {
-		return "morse code"
-	}
-
-	return "mistake input string"
-}
-
-func convertToStringByMorse(strInput string) string{
-	// Morse Code to String
-
-	return "mistake input string"
-}
+var morseData map[string]interface{}
 
 func main() {
-	/*
-	if len(os.Args) < 2 {
-		fmt.Printf("usage: %s String or Morse Code\n", os.Args[0])
-		os.Exit(-1)
+	//code := flag.String("code", "", "input your morse code")
+	word := flag.String("word", "", "input your words")
+
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		return
 	}
 
-	strInput := os.Args[1]
-
-	fmt.Println(convertToMorseByString(strInput))
-	*/
-
-	var data map[string]interface{}
 	file, err := ioutil.ReadFile("./morseData.json")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = json.Unmarshal(file, &data)
+
+	err = json.Unmarshal(file, &morseData)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	//fmt.Println(data)
-	strInput := os.Args[1]
-	fmt.Println(data[strInput])
 
-	/*
-	file, e := ioutil.ReadFile("./test.json")
-
-	if (e != nil) {
-		fmt.Printf("File error: %v\n", e)
-		os.Exit(1)
-	}
-
-
-	fmt.Println(string(file))
-
-	var yourJson YourJson
-	json.Unmarshal(file, &yourJson)
-	fmt.Printf("Results: %v\n", yourJson)
-
-	dec := json.NewDecoder(bytes.NewReader(file))
-	var d YourJson
-	fmt.Println(dec.Decode(&d))
-	*/
+	fmt.Println(morseData[*word])
 }
--- morse_test.go
+++ morse_test.go
@@ -1,22 +1,25 @@
 package main
+
 import (
 	"testing"
 	"io/ioutil"
+	"log"
 	"encoding/json"
-	"bytes"
-	"fmt"
 )
 
-type YourJson struct {
-	YourSample []struct {
-		data map[string]string
-	}
-}
-
 func TestParseToJsonData(t *testing.T){
-	c, _ := ioutil.ReadFile("test")
 
-	dec := json.NewDecoder(bytes.NewReader(c))
-	var d YourJson
-	fmt.Println(dec.Decode(&d))
+	file, err := ioutil.ReadFile("./test.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(file, &morseData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(morseData)
+
 }
