2c583f98f1a368050dda1ea3ba674ecd41a0ee50
YoonJungBoo <coma333ryu@gmail.com>
2015-09-29 15:43:01 -0400

    no message

--- morse_test.go
+++ morse_test.go
@@ -3,10 +3,6 @@ package main
 import (
 	"testing"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"encoding/json"
 )
 
 type MorseStruct struct {
@@ -19,43 +15,16 @@ type MorseStruct struct {
 }
 
 func TestParseToJsonData(t *testing.T){
+	strFileName = "./test.json"
+	readJsonFile(strFileName)
 
-	//readJsonFile("./test.json")
-
-	file, err := ioutil.ReadFile("./test.json")
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(-1)
-	}
-
-	//var morseStruct = new(MorseStruct)
-	//morseUSData: make(map[string]interface{})
-	var testMap map[string] map[string] interface{}
-	err = json.Unmarshal(file, &testMap)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(-1)
-	}
-
-	//fmt.Println(MorseStruct.MorseUS.morseUSDatas["a"])
+	//fmt.Println(MorseStruct.MorseUS.morseUSData["a"])
 	//strMorseResult += morseData[strWord[idx:idx+1]].(string) + " "
-	morseUSCode := testMap["morseUS"]["d"]
+	morseUSCode := morseData["morseUS"]["c"]
 
 	if(morseUSCode == nil){
 		fmt.Println("nil")
 	}else{
 		fmt.Println(morseUSCode)
 	}
-
-
-	str := "a*"
-	fmt.Println("str ====>", str)
-
-	for _, r := range str{
-		fmt.Println(r)
-	}
-
-
-
 }
