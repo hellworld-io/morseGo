aa4b2eba09413d363e83489c712766aacc22434d
YoonJungBoo <coma333ryu@gmail.com>
2015-09-30 19:16:16 -0400

    code refactoring

--- morse.go
+++ morse.go
@@ -22,24 +22,30 @@ var bMorseToAlphabet = flag.Bool("mta", false, "To need morse codes for alphabet
 func makeToStringByArgsAlphabet(strArgs []string) string {
 	var strArgsResult string
 
-	if(*bAlphabetToMorse == true){
-		for _, argument := range strArgs {
-			strArgsResult += argument
-		}
-
-		strArgsResult = strings.ToLower(strArgsResult)
-	}else if(*bMorseToAlphabet == true){
-		for _, argument := range strArgs {
-			strArgsResult += argument
-		}
-	}else{
-		log.Fatal("Arguments error !!! no flag")
-		os.Exit(-1)
-	}
-
 	if (len(strArgs) == 0){
 		log.Fatal("Arguments is null !!!")
 		os.Exit(-1)
+	}
+
+	if(*bAlphabetToMorse != true && *bMorseToAlphabet != true){
+		log.Fatal("option error !!! no flag")
+		os.Exit(-1)
+	}
+
+	/*
+	if(*bAlphabetToMorse == true && *bMorseToAlphabet == true){
+		log.Fatal("option error !!! double flag")
+		os.Exit(-1)
+	}
+	*/
+
+	fmt.Println("strArgs ", strArgs[0])
+	for _, argument := range strArgs {
+		strArgsResult += argument
+	}
+
+	if(*bAlphabetToMorse == true){
+		strArgsResult = strings.ToLower(strArgsResult)
 	}
 
 	return strArgsResult
@@ -64,7 +70,7 @@ func convertToByArgs(strWord string) string{
 			}
 
 			if(morseData["morseWordsUS"][arrMorseWord[idx]] == nil){
-				log.Fatal("There are no matching words.")
+				log.Fatal("There are no matching morse codes.")
 				os.Exit(-1)
 			}
 			strMorseResult += morseData["morseWordsUS"][arrMorseWord[idx]].(string)
@@ -92,6 +98,8 @@ func readJsonFile(strFileName string) {
 func main() {
 	flag.Parse()
 
+	fmt.Println("bAlphabetToMorse ", *bAlphabetToMorse)
+	fmt.Println("bMorseToAlphabet ", *bMorseToAlphabet)
 	var strArgs string
 
 	strArgs = makeToStringByArgsAlphabet(flag.Args())
