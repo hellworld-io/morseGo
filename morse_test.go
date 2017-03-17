d65c77c27183111f9b53b6e97175c38f761ac756
YoonJeongbu <coma333ryu@gmail.com>
2017-03-17 11:33:56 +0900

    sources modified

--- morse_test.go
+++ morse_test.go
@@ -6,8 +6,15 @@ import (
 )
 
 func TestParseToJsonData(t *testing.T){
-	strFileName = "./test.json"
+	//strFileName := "./test.json"
 
 	//jsonUtils.ReadJsonData(strFileName)
 	//fmt.Printf("Results: %v\n", jsontype)
+
+	/* get struct field value
+	todoData2 := reflect.ValueOf(todoParam).Elem()
+	fmt.Println("todoData2 ======> ", todoData2)
+	value := todoData2.FieldByName("totoTitle")
+	fmt.Println("value============> ", value)
+	*/
 }
