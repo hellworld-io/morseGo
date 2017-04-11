ef46ece6471ae7a084ffa376a18ee4129332c756
YoonJeongbu <coma333ryu@gmail.com>
2017-04-11 13:35:21 +0900

    change function name from MorseMessage to defaultMessage

--- flags/morseFlags.go
+++ flags/morseFlags.go
@@ -1,36 +1,36 @@
 package flags
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"morseGo/constants"
 	"os"
 	"strings"
 )
 
 var (
-	convertCmd = flag.NewFlagSet("convert",flag.ExitOnError)
-	convertText = convertCmd.String("text","","Text to morse code ex) -text 'a b'")
-	convertMorse = convertCmd.String("morse","","morse code to text ex) -morse '. .-  . .-'")
+	convertCmd   = flag.NewFlagSet("convert", flag.ExitOnError)
+	convertText  = convertCmd.String("text", "", "Text to morse code ex) -text 'a b'")
+	convertMorse = convertCmd.String("morse", "", "morse code to text ex) -morse '. .-  . .-'")
 )
 
-func MorseMessage() {
+func defaultMessage() {
 	fmt.Println(constants.MorseUsage)
 	flag.PrintDefaults()
 }
 
-func SetFlags() (textArgs string, morseArg string){
-	flag.Usage = MorseMessage
+func SetFlags() (textArgs string, morseArg string) {
+	flag.Usage = defaultMessage
 
 	if len(os.Args) < 2 {
-		MorseMessage()
+		defaultMessage()
 		os.Exit(-1)
 	}
 
 	if os.Args[1] == "convert" {
 		convertCmd.Parse(os.Args[2:])
 	} else {
-		MorseMessage()
+		defaultMessage()
 		os.Exit(-1)
 	}
 
